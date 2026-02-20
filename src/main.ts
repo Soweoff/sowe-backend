3792b3e76e6b8d73508a1db7096f4ef9129be190
Soweoff <evertonsouzawience@gmail.com>
2026-02-20 18:23:33 -0300

    preparado para produção deploy 2

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'https://sowestudio.com.br/',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
