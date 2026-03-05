52fcad7e71e2b7e95daabeba376defae84f3764a
Soweoff <evertonsouzawience@gmail.com>
2026-03-04 23:13:49 -0300

    API Backend monday

--- src/monday/monday.module.ts
+++ src/monday/monday.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MondayController } from './monday.controller';
 import { MondayService } from './monday.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [MondayController],
   providers: [MondayService],
 })
--- src/monday/monday.service.ts
+++ src/monday/monday.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MondayService {
+  constructor(private configService: ConfigService) {}
+
   async getTasks() {
+    const token = this.configService.get<string>('MONDAY_TOKEN');
+
     const query = `
       query {
         boards(ids: 18402272661) {
-          items_page {
+          items_page(limit: 50) {
             items {
               id
               name
@@ -22,7 +27,8 @@ export class MondayService {
       { query },
       {
         headers: {
-          Authorization: process.env.MONDAY_TOKEN,
+          Authorization: token,
+          'Content-Type': 'application/json',
         },
       },
     );
